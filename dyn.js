a8efc52bd4da378f5611a5d943a2c1854288dafa
John Voorhess <voorhessjohn@gmail.com>
2018-07-31 14:41:54 -0400

    writes variables from dynamodb to browser window

--- dyn.js
+++ dyn.js
@@ -1,3 +1,4 @@
+var http = require("http");
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
 // Set the region 
@@ -26,4 +27,17 @@ var Emotion_1 = data.Item.Emotion_1.S
 var Confidence_1 = data.Item.Confidence_1.N
 console.log(Emotion_1,Confidence_1)
 
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end(Emotion_1+" "+Confidence_1);
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
 });
